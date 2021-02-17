142b339db25be95b318d53921cf0b69f10274f2c
Abhinav Agrawal <abhinav.hfs@gmail.com>
2021-02-17 08:58:30 +0530

    Modified HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -5,6 +5,6 @@ int main(int argc, char const *argv[])
 	int num;
 	printf("Enter Number: \n");
 	scanf("%d", &num);
-	printf("Number Entered: %d\n", num);
+	printf("Number Entered: %d\n", (num+1);
 	return 0;
 }
